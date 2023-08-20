5adfe599999597e6ad77f385c306d97c4cc37465
vpg10k <konstantindmitriev29@gmail.com>
2023-08-20 21:16:38 +0300

    teminal HW2 progress

---
+++ dir_1/inner_dir_1/13strings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+touch tF_5.txt
+for (( i=1; i <= 13; i++ ))
+do
+	echo "string number $i" >> tF_5.txt
+done
+
---
+++ dir_1/inner_dir_1/15strings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+touch tf_4.txt
+for (( i=1; i <= 15; i++))
+do
+echo "string number $i" >> tf_4.txt
+done
+
